ebe672acb165c1cafac3303d5798f0b01d38030b
JamesKam1995 <kamsayhong@gmail.com>
2024-10-19 13:57:03 +0800

    amend my_first_node.cpp and my and first_node.py

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -3,23 +3,24 @@
 class MyNode : public rclcpp::Node
 {
 public:
-        MyNode() : Node("cpp_test"), counter_(0)
-        {
-            RCLCPP_INFO(this -> get_logger(), "Hello Cpp Node");
+    MyNode() : Node("cpp_test"), counter_(0)
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello Cpp Node");
 
-            timer_ = this -> create_wall_timer(std::chrono::seconds(1),
-                                                std::bind(&MyNode::timerCallback, this));
-        }
+        timer_ = this->create_wall_timer(std::chrono::seconds(1),
+                                         std::bind(&MyNode::timerCallback, this));
+    }
 
 private:
-        void timerCallback()
-        {
-            counter_++
-            RCLCPP_INFO(this-> get logger(), "Hello %d", counter_)
-        }
-        rclcpp::TimerBase::SharedPtr timer_;
-        int counter_;
-}
+    void timerCallback()
+    {
+        counter_++;
+        RCLCPP_INFO(this->get_logger(), "Hello %d", counter_);
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    int counter_;
+};
 
 int main(int argc, char **argv)
 {
@@ -29,4 +30,3 @@ int main(int argc, char **argv)
     rclcpp::shutdown();
     return 0;
 }
-
